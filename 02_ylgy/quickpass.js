5f3be1568adb77cc534702195c69d402d7694f93
powerjiayun <powerjiayunmei@gmail.com>
2022-09-20 22:42:07 +0800

    [*] 2022-09-20 适配

--- 02_ylgy/quickpass.js
+++ 02_ylgy/quickpass.js
@@ -1,10 +1,14 @@
-var body = $response.body
-
-if (body) {
-  var obj = JSON.parse($response.body)
-  obj.data = '046ef1bab26e5b9bfe2473ded237b572'
-    // replace with the data in level 1
-  $done({ body: JSON.stringify(obj) }) // success
-} else {
-  $done({}) // fail
+var Body = JSON.parse($response.body);
+switch ($request.url.match(/map|personal|json/)[0]){
+  case "map":
+    Body.data.map_md5[1] = '046ef1bab26e5b9bfe2473ded237b572';
+    break;
+  case "personal":
+    var objk = Body.data;
+    objk["daily_count"] = "1e+308";
+    objk["nick_name"] = "金字塔的顶端";
+    break;
+  default:
+    $done({ body: $response.body.replace(/false/g,'true') });
 }
+$done({body: JSON.stringify(Body)});
